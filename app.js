399a0346204000dfcfccdf46788c2fe4e6657dc2
sachindsilva16 <sachindsilva16@gmail.com>
2023-09-19 11:26:54 +0530

    add post request for new register

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const { log } = require("console");
 
 const app = express();
 
@@ -16,20 +17,48 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://127.0.0.1:27017/usersDB", { useNewUrlParser: true });
 
-app.get("/",(req,res)=>{
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
     res.render("home");
 });
 
 
-app.get("/register",(req,res)=>{
+app.get("/register", (req, res) => {
     res.render("register");
 });
 
 
-app.get("/login",(req,res)=>{
+app.get("/login", (req, res) => {
     res.render("login");
 });
 
 
+app.post("/register", (req, res) => {
 
-app.listen(PORT,()=>{console.log(`Your server started running on port ${PORT}`)});
+    const inputEmail = req.body.username;
+    const inputPassword = req.body.password;
+
+    const newUser = new User({
+        email: inputEmail,
+        password: inputPassword
+    });
+
+    newUser.save().then(() => {
+        console.log("User is added succesfully to the database");
+        res.render("secrets");
+    }).catch(err => {
+        console.log(err);
+    });
+});
+
+
+
+app.listen(PORT, () => { console.log(`Your server started running on port ${ PORT }`) });
